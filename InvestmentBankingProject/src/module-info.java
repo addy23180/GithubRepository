fe6c7f83fa1a720fe3f040475d4f2caa4281e923
addy23180 <113611691+addy23180@users.noreply.github.com>
2022-09-15 22:32:12 +0530

    Homepage feature completed
    First commit

---
+++ InvestmentBankingProject/src/module-info.java
@@ -0,0 +1,2 @@
+module InvestmentBankingProject {
+}
---
+++ InvestmentBankingProject/src/package1/GithubHomepage.java
@@ -0,0 +1,8 @@
+package package1;
+
+public class GithubHomepage {
+	public static void main(String [] args) {
+		System.out.println("Github Homepage");
+	}
+
+}
